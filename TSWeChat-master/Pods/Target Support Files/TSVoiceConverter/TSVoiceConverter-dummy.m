25bc117f9219041f7d3158a3bf7a8fc4c0e0bb10
Minh Tri <trminh36@gmail.com>
2020-12-26 15:49:17 +0700

    remove unnesscesary file

--- TSWeChat-master/Pods/Target Support Files/TSVoiceConverter/TSVoiceConverter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TSVoiceConverter : NSObject
-@end
-@implementation PodsDummy_TSVoiceConverter
-@end
--- TSWeChat-master/Pods/Target Support Files/TimedSilver/TimedSilver-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TimedSilver : NSObject
-@end
-@implementation PodsDummy_TimedSilver
-@end
